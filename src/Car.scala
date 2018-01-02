4e8ab3a62d3943a20c7633bce88a3efbb1819c24
Tomasz Janaszka <tomasz.janaszka@orange.com>
2018-01-02 15:16:14 +0100

    added new files after the course completion

---
+++ src/Car.scala
@@ -0,0 +1,10 @@
+class Car(milesPerGalon:Int, gallonsPerTank:Int) {
+  var mpg: Int = milesPerGalon
+  var gpt: Int = gallonsPerTank
+  var mpt: Int = 0
+
+  def milesPerTank(): Unit ={
+    mpt = mpg * gpt
+    println("Number of Miles Per Tank:" +mpt)
+  }
+}
--- src/Volvo.scala
+++ src/Volvo.scala
@@ -1,14 +1,3 @@
-class Car(milesPerGalon:Int, gallonsPerTank:Int) {
-  var mpg: Int = milesPerGalon
-  var gpt: Int = gallonsPerTank
-  var mpt: Int = 0
-
-  def milesPerTank(): Unit ={
-    mpt = mpg * gpt
-    println("Number of Miles Per Tank:" +mpt)
-  }
-}
-
 object Volvo {
   def main(args: Array[String]) = {
     val volvo = new Car(40, 18)
