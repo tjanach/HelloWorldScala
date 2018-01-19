341039304b1a45f1c78a0e5bec5acf61ab10844e
Tomasz Janaszka <tomasz.janaszka@orange.com>
2018-01-19 14:08:27 +0100

    update to streamsDemo.scala

--- src/main/scala/streamsDemo.scala
+++ src/main/scala/streamsDemo.scala
@@ -34,10 +34,7 @@ object streamsDemo {
 
     // Generate a random string of length n from the given alphabet
     def randomString(alphabet: String)(n: Int): String =
-      Stream.continually(random.nextInt(alphabet.size)).map(alphabet).take(n).mkString
-
-
-
+      Stream.continually(random.nextInt(alphabet.length)).map(alphabet).take(n).mkString
 
     // Generate a random alphabnumeric string of length n
     def randomAlphanumericString(n: Int) =
